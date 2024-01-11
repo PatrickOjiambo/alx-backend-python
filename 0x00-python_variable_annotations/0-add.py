391763e04557c2d9beb9fe5efa87d01ebb312832
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-11 16:21:34 +0300

    feat: task 2

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,7 @@ Code for type annotated add
 
 
 def add(a: float, b: float) -> float:
+    """
+    Function for annotated add
+    """
     return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+File to return floor for some decimal
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Function to return floor for some decimal
+    """
+    return math.floor(n)
