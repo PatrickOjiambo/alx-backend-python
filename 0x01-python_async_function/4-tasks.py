fe2bacef2684988b744e11cfc7fdee8192bdcd4c
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-15 23:38:41 +0300

    feat: task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -16,7 +16,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Import wait_random from the previous python file that
     you’ve written and write an async routine called wait_n
