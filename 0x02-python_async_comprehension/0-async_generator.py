46b92de3066d4c569611e97066732461e448f581
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-16 21:24:10 +0300

    feat: task 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Write a coroutine called async_generator that takes no arguments.
 """
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator() -> None:
+async def async_generator() -> Generator[float, None, None]:
     """
     Write a coroutine called async_generator that takes no arguments.
     """
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,13 +3,14 @@
 Import async_generator from the previous task and then
 write a coroutine called async_comprehension that takes no arguments.
 """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> None:
+async def async_comprehension() -> List[float]:
     """
     Import async_generator from the previous task and then
     write a coroutine called async_comprehension that takes no arguments.
     """
-    the_numbers = [i async for i in async_generator]
-    print(the_numbers)
+    the_numbers = [i async for i in async_generator()]
+    return the_numbers
